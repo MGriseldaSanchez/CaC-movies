e0d4ac756b754917df96ec126ec40de4a3ec030f
MGSanchez <griseldamsanch@gmail.com>
2024-04-24 23:54:48 -0300

    test en API

--- js/API.js
+++ js/API.js
@@ -1,31 +1,60 @@
-//API 
-
+//API the movieDB
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json'
-    //   Authorization: 'e99484e5690c9db020b0d59b239061bf'
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTk0ODRlNTY5MGM5ZGIwMjBiMGQ1OWIyMzkwNjFiZiIsInN1YiI6IjY2MjU4NTZjNjNlNmZiMDE2NWZiYzlmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MlBIoferekcyTO8Oeb0Oyvuso3Os2anfe8tp7sTyb0s'
     }
   }
-
-const apiKey = 'e99484e5690c9db020b0d59b239061bf';
-const region = 'US'; // Puedes ajustar esto según tu región para obtener películas de estreno en tu país
-const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&region=${region}`;
-
-
+const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
 
   
+// traer contenedor de las peliculas 
+const films = document.getElementById("content-films")
+
+// funcion crea etiqueta
+const createTag = (tag) => {
+    const element = document.createElement(tag)
+    return element
+    //! MODIFICAR
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   fetch(url, options)
-//   https://api.themoviedb.org/3/movie/popular
-//https://api.themoviedb.org/3/search/movie/recommendations?api_key=e99484e5690c9db020b0d59b239061bf
     .then(response => response.json())
-    // .then(response => console.log(response))
+   
 
     .then(data => {
 
-        console.log(data)
+      //   console.log(data.results[0].poster_path)
 
-    })
+      // data.results.forEach(poster => {
+
+      //   const movieDiv = document.createElement('div')
+      //   const img = document.createElement('img')
+      //   img.src = poster_path
+
+      //   movieDiv.appendChild(img)
+      //   moviesContainer.appendChild(movieDiv)
+
+      console.log(data)
+        
+      })
+
+
 
     .catch(err => console.error(err))
 
